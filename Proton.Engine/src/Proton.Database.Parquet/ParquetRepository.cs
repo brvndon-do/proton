c0d275e2e0fca96acc1cfe608c0ce499eff04d77
Brandon Do <do.brandon97@gmail.com>
2026-07-25 23:46:25 -0400

    handle concurrent reads/writes

--- Proton.Engine/src/Proton.Database.Parquet/ParquetRepository.cs
+++ Proton.Engine/src/Proton.Database.Parquet/ParquetRepository.cs
@@ -1,67 +1,115 @@
-﻿using Parquet.Serialization;
+﻿using System.Collections.Concurrent;
+using Parquet.Serialization;
 using Proton.Engine.Core.Interfaces.Repositories;
 using Proton.Engine.Core.Models;
 
 namespace Proton.Engine.Database.Parquet;
 
-// TODO: this will probably need to be thread safe if multiple requests from MarketDataSubscriptionManager
-//       or BacktestingService will need to read/write from the same file(s).
-
 public class ParquetRepository : IBarRepository
 {
     private static readonly string PARQUET_FILE_DIR = Path.Combine(AppContext.BaseDirectory, "output");
+    private static readonly ConcurrentDictionary<string, SemaphoreSlim> _fileLocks = new ConcurrentDictionary<string, SemaphoreSlim>();
+
+    private static SemaphoreSlim LockFile(string symbol) => _fileLocks.GetOrAdd(symbol, _ => new SemaphoreSlim(initialCount: 1, maxCount: 1));
 
     public async Task AddAsync(Bar entity, CancellationToken cancellationToken = default)
     {
-        (FileStream fs, bool exists) = GetFileStream(entity.Symbol);
+        SemaphoreSlim gate = LockFile(entity.Symbol);
+        await gate.WaitAsync(cancellationToken);
 
-        using (fs)
-            await ParquetSerializer.SerializeAsync(
-                objectInstances: [entity],
-                destination: fs,
-                options: new ParquetSerializerOptions { Append = exists },
-                cancellationToken: cancellationToken
-            );
-    }
-
-    public async Task AddRangeAsync(IEnumerable<Bar> entities, CancellationToken cancellationToken = default)
-    {
-        IEnumerable<IGrouping<string, Bar>> sortedEntities = entities.GroupBy(x => x.Symbol);
-
-        foreach (IGrouping<string, Bar> bars in sortedEntities)
+        try
         {
-            (FileStream fs, bool exists) = GetFileStream(bars.Key);
+            (FileStream fs, bool exists) = GetFileStream(entity.Symbol);
 
             using (fs)
+            {
                 await ParquetSerializer.SerializeAsync(
-                    objectInstances: bars,
+                    objectInstances: [entity],
                     destination: fs,
                     options: new ParquetSerializerOptions { Append = exists },
                     cancellationToken: cancellationToken
                 );
+            }
+        }
+        finally
+        {
+            gate.Release();
+        }
+    }
+
+    public async Task AddRangeAsync(IEnumerable<Bar> entities, CancellationToken cancellationToken = default)
+    {
+        foreach (IGrouping<string, Bar> bars in entities.GroupBy(x => x.Symbol))
+        {
+            SemaphoreSlim gate = LockFile(bars.Key);
+            await gate.WaitAsync(cancellationToken);
+
+            try
+            {
+                (FileStream fs, bool exists) = GetFileStream(bars.Key);
+
+                using (fs)
+                {
+                    await ParquetSerializer.SerializeAsync(
+                        objectInstances: bars,
+                        destination: fs,
+                        options: new ParquetSerializerOptions { Append = exists },
+                        cancellationToken: cancellationToken
+                    );
+                }
+            }
+            finally
+            {
+                gate.Release();
+            }
         }
     }
 
     public async Task RemoveByKeyAsync(string key, CancellationToken cancellationToken = default)
     {
-        (FileStream _, bool fileExists) = GetFileStream(key);
+        SemaphoreSlim gate = LockFile(key);
+        await gate.WaitAsync(cancellationToken);
 
-        if (!fileExists)
-            return;
+        try
+        {
+            string path = Path.Combine(PARQUET_FILE_DIR, $"{key}.parquet");
 
-        // TODO: seems dangerous to remove file fully. look into different ways of preserving.
-        File.Delete(Path.Combine(PARQUET_FILE_DIR, $"{key}.parquet"));
+            if (File.Exists(path))
+                File.Delete(path);
+        }
+        finally
+        {
+            gate.Release();
+        }
     }
 
     public async Task<IEnumerable<Bar>> ReadBarsAsync(string symbol, CancellationToken cancellationToken = default)
     {
-        (FileStream fs, _) = GetFileStream(symbol);
+        IList<Bar> barsRead = [];
+        SemaphoreSlim gate = LockFile(symbol);
+        await gate.WaitAsync(cancellationToken);
 
-        using (fs)
-            return await ParquetSerializer.DeserializeAsync<Bar>(fs, cancellationToken: cancellationToken);
+        try
+        {
+            (FileStream fs, _) = GetFileStream(symbol, write: false);
+
+            using (fs)
+            {
+                if (fs.Length == 0)
+                    return [];
+
+                barsRead = await ParquetSerializer.DeserializeAsync<Bar>(fs, cancellationToken: cancellationToken);
+            }
+        }
+        finally
+        {
+            gate.Release();
+        }
+
+        return barsRead;
     }
 
-    private (FileStream fileStream, bool fileExists) GetFileStream(string filename)
+    private (FileStream fileStream, bool fileExists) GetFileStream(string filename, bool write = true)
     {
         if (!Directory.Exists(PARQUET_FILE_DIR))
             Directory.CreateDirectory(PARQUET_FILE_DIR);
@@ -72,7 +120,7 @@ public class ParquetRepository : IBarRepository
         FileStream fs = new FileStream(
             path,
             FileMode.OpenOrCreate,
-            FileAccess.ReadWrite
+            write ? FileAccess.ReadWrite : FileAccess.Read
         );
 
         return (
